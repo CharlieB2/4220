6761fca04bc4c4c1da78d9c17b93d4df69a44124
CharlieB2 <93560163+CharlieB2@users.noreply.github.com>
2023-03-09 17:49:55 -0800

    Update cli.js
    Base CLI copied

--- cli.js
+++ cli.js
@@ -1,1 +1,41 @@
+const yargs = require('yargs/yargs');
 
+
+const app = require('./app.js');
+
+
+yargs(process.argv.slice(2))
+    // $0 expands the name of the file
+    // <> indicate the command is mandatory
+    // [] indicate the command options are optional
+    .usage('$0: Usage <command> [options]')
+    .command(
+        // command
+        // <> indicate the command argument is mandatory
+        'getweapons',
+        // description
+        'Used to get weapons from the game',
+        // builder
+        (yargs) => {
+            return yargs    
+                .option('weapontype', {
+                    alias: 'wt',
+                    describe: 'returns a list of weapons that match the keyword',
+                    default: empty list,
+                    type: 'string'
+                });
+        },
+        // handler
+        (args) => {
+            if (args.id === string) {
+                // returns a list of weapons
+
+
+		    // app.getWeapons()
+                app.playPoker(args);
+            } else {
+                console.log(“not a valid input”);
+            }
+        }
+    )
+    .help().argv;
