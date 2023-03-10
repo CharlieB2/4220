605315f2b48e946d547b199a3eac48bac7150b26
CharlieB2 <93560163+CharlieB2@users.noreply.github.com>
2023-03-09 17:52:35 -0800

    Update app.js
    Cleaned up indentations

--- app.js
+++ app.js
@@ -1,33 +1,26 @@
 const superagent = require('superagent');
-
-
-// ----- TODO Week 08 - refactor this into api.js
-
-
 const base = 'https://mhw-db.com';
-
 
 Const getWeapon =  async (weaponType) => {
 	Try{
 		Const drawUrl = `${base}/weapons?q={‘type’ : ${weaponType}}`;
-	      // Below is another possible option for searching weapon types
-      // `${base}/weapons?q={“weapons.type”:${weaponType}}`;
+		// Below is another possible option for searching weapon types
+		// `${base}/weapons?q={“weapons.type”:${weaponType}}`;
 		Const res = await superagent.get(drawurl);
-            console.log(res);	
-} catch (error){
-      	console.log(error);
+		console.log(res);	
+	} catch (error){
+      		console.log(error);
 	}
 }
-
 
 Const selectWeapon = async (id) => {
 	Try{
 		Const drawUrl = ‘${base}/weapons/${id}’;
-	      Const res = await superagent.get(drawUrl);
+		Const res = await superagent.get(drawUrl);
 		console.log(res);
 	} catch (error) {
-		console.log(error)
-}
+		console.log(error);
+	}
 
 
 }
